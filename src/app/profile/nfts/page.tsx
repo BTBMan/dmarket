67c876174838b13de2bc88f820952d562e5a4095
BTBMan <1498118157@qq.com>
2025-03-10 16:32:23 +0800

    chore: add dialog shadcn component
    Signed-off-by: BTBMan <1498118157@qq.com>

--- src/app/profile/nfts/page.tsx
+++ src/app/profile/nfts/page.tsx
@@ -1,8 +1,12 @@
 import NFTCard from '@/components/NFTCard'
+import NFTForm from '@/components/NFTForm'
 
 export default function NftsPage() {
   return (
     <div>
+      <div className="flex justify-end mb-[var(--main-padding)]">
+        <NFTForm />
+      </div>
       <div className="grid grid-cols-5 gap-4 max-xl:grid-cols-4">
         { Array.from({ length: 10 }).map((_, index) => (
           <NFTCard key={index} />
