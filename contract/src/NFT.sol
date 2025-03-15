dc284727e1e8cd4d2cd9a335947b1eff98485405
BTBMan <1498118157@qq.com>
2025-03-15 12:01:51 +0800

    wip: nft contract
    Signed-off-by: BTBMan <1498118157@qq.com>

--- contract/src/NFT.sol
+++ contract/src/NFT.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.27;
 
 /* Imports *******/
-import { ERC721URIStorage } from '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
-
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 /* Events ********/
 
 /* Errors ********/
@@ -18,37 +18,48 @@ import { ERC721URIStorage } from '@openzeppelin/contracts/token/ERC721/extension
  * @notice This is a NFT contract
  */
 contract NFT is ERC721URIStorage {
-  ////////////////////////////////////
-  // Type declarations              //
-  ////////////////////////////////////
+    ////////////////////////////////////
+    // Type declarations              //
+    ////////////////////////////////////
 
-  ////////////////////////////////////
-  // State variables                //
-  ////////////////////////////////////
+    ////////////////////////////////////
+    // State variables                //
+    ////////////////////////////////////
+    uint256 private s_tokenIds;
+    address private s_nftMarketContract;
 
-  ////////////////////////////////////
-  // Events                         //
-  ////////////////////////////////////
+    ////////////////////////////////////
+    // Events                         //
+    ////////////////////////////////////
 
-  ////////////////////////////////////
-  // Errors                         //
-  ////////////////////////////////////
+    ////////////////////////////////////
+    // Errors                         //
+    ////////////////////////////////////
 
-  ////////////////////////////////////
-  // Modifiers                      //
-  ////////////////////////////////////
+    ////////////////////////////////////
+    // Modifiers                      //
+    ////////////////////////////////////
 
-  constructor() {}
+    constructor(address nftMarketContract) ERC721("Awesome NFT", "AN") {
+        s_nftMarketContract = nftMarketContract;
+    }
 
-  ////////////////////////////////////
-  // Receive & Fallback             //
-  ////////////////////////////////////
+    ////////////////////////////////////
+    // Receive & Fallback             //
+    ////////////////////////////////////
 
-  ////////////////////////////////////
-  // Functions                      //
-  ////////////////////////////////////
+    ////////////////////////////////////
+    // Functions                      //
+    ////////////////////////////////////
+    function mintNFT(string memory tokenURI) public returns (uint256) {
+        _mint(msg.sender, s_tokenIds);
+        _setTokenURI(s_tokenIds, tokenURI);
+        setApprovalForAll(s_nftMarketContract, true); // Allow NFT Market to manage all of minter's tokens
+        s_tokenIds++;
+        return s_tokenIds;
+    }
 
-  ////////////////////////////////////
-  // Getter functions               //
-  ////////////////////////////////////
+    ////////////////////////////////////
+    // Getter functions               //
+    ////////////////////////////////////
 }
