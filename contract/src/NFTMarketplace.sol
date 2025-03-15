a4e7b431f18dba9c0e92da4aac32da8f482c473f
BTBMan <1498118157@qq.com>
2025-03-15 13:24:50 +0800

    wip: createMarketItem
    Signed-off-by: BTBMan <1498118157@qq.com>

--- contract/src/NFTMarketplace.sol
+++ contract/src/NFTMarketplace.sol
@@ -13,35 +13,50 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 /* Libraries *****/
 
 /**
- * @title NFT
+ * @title NFTMarketplace
  * @author BTBMan
- * @notice This is a NFT contract
+ * @notice This is a contract for a marketplace for NFTs
  */
-contract NFT is ERC721URIStorage {
+contract NFTMarketplace is ERC721URIStorage {
     ////////////////////////////////////
     // Type declarations              //
     ////////////////////////////////////
+    struct MarketItem {
+        uint256 tokenId;
+        uint256 price;
+        address payable seller;
+        address payable owner;
+        bool sold;
+    }
 
     ////////////////////////////////////
     // State variables                //
     ////////////////////////////////////
+    uint256 private constant LISTING_FEE = 0.01 ether;
+
+    address payable private s_owner;
     uint256 private s_tokenIds;
-    address private s_nftMarketContract;
+    uint256 private s_marketItemIds;
+    mapping(uint256 marketItemId => MarketItem marketItem) private s_marketItems;
 
     ////////////////////////////////////
     // Events                         //
     ////////////////////////////////////
+    event MarketItemCreated(
+        uint256 indexed marketItemId, uint256 indexed tokenId, address seller, address owner, uint256 price, bool sold
+    );
 
     ////////////////////////////////////
     // Errors                         //
     ////////////////////////////////////
+    error NFTMarketplace__NotEnoughListingFeePaid();
 
     ////////////////////////////////////
     // Modifiers                      //
     ////////////////////////////////////
 
-    constructor(address nftMarketContract) ERC721("Awesome NFT", "AN") {
-        s_nftMarketContract = nftMarketContract;
+    constructor() ERC721("Awesome NFT", "AN") {
+        s_owner = payable(msg.sender);
     }
 
     ////////////////////////////////////
@@ -54,9 +69,21 @@ contract NFT is ERC721URIStorage {
     function mintNFT(string memory tokenURI) public returns (uint256) {
         _mint(msg.sender, s_tokenIds);
         _setTokenURI(s_tokenIds, tokenURI);
-        setApprovalForAll(s_nftMarketContract, true); // Allow NFT Market to manage all of minter's tokens
         s_tokenIds++;
         return s_tokenIds;
+    }
+
+    function createMarketItem(uint256 price, string memory tokenURI) public payable {
+        if (price < LISTING_FEE) {
+            revert NFTMarketplace__NotEnoughListingFeePaid();
+        }
+
+        uint256 tokenId = mintNFT(tokenURI);
+
+        s_marketItems[s_marketItemIds] = MarketItem(tokenId, price, payable(msg.sender), payable(address(0)), false);
+        s_marketItemIds++;
+
+        emit MarketItemCreated(s_marketItemIds, tokenId, msg.sender, address(0), price, false);
     }
 
     ////////////////////////////////////
